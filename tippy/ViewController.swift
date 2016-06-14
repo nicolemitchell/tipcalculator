30d15f7de0f6b63ff094ad7b1e84f50862740895
Nicole Mitchell <nicolemitchell@fb.com>
2016-06-14 13:13:22 -0700

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Nicole Mitchell on 6/14/16.
+//  Copyright © 2016 Nicole Mitchell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
